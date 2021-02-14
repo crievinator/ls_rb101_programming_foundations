303bb374bf2756e283072a7476c971c18b74f14b
Crievino <creemersyves@gmail.com>
2021-02-14 19:52:44 +0100

    changes for rock_paper_scissor.rb

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,26 +1,23 @@
 VALID_CHOICES = %w[paper rock scissors lizard spock]
+WINNING_COMBINATIONS = {
+  'rock' => %w[scissors lizard],
+  'paper' => %w[spock rock],
+  'scissors' => %w[paper lizard],
+  'lizard' => %w[paper spock],
+  'spock' => %w[rock scissors]
+}
 
-def test_method
-  prompt('test message')
-end
-
+# method to output program messages with a '=>'
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+# determine in combination (first vs. second) if first has one
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'rock' && second == 'lizard') ||
-    (first == 'paper' && second == 'spock') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper') ||
-    (first == 'scissors' && second == 'lizard') ||
-    (first == 'lizard' && second == 'paper') ||
-    (first == 'lizard' && second == 'spock') ||
-    (first == 'spock' && second == 'rock') ||
-    (first == 'spock' && second == 'scissors')
+  WINNING_COMBINATIONS[first].include?(second)
 end
 
+# display which player has won
 def display_results(player, computer)
   if win?(player, computer)
     prompt('You won!')
@@ -31,6 +28,7 @@ def display_results(player, computer)
   end
 end
 
+# translate abbreviation to full name from VALID_CHOICES constant
 def translate_choice(abr)
   case abr
   when 'p' then 'paper'
@@ -41,47 +39,43 @@ def translate_choice(abr)
   end
 end
 
-def update_players_score(player, computer, players_scores, player_wins, computer_wins)
+# update the player's scores in a hash
+def update_players_score(player, computer, players_scores)
   if win?(player, computer)
     players_scores[:player] += 1
-    p player_wins += 1
   elsif win?(computer, player)
     players_scores[:computer] += 1
-    p computer_wins += 1
   end
 end
 
 players_scores = Hash.new(0)
-player_wins = 0
-computer_wins = 0
+
 loop do
   choice = ''
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')} by typing p, r, s, l or sp")
+
     abreviated_choice = Kernel.gets.chomp
     choice = translate_choice(abreviated_choice)
-
     break if VALID_CHOICES.include?(choice)
 
     prompt("That's not a valid choice.")
   end
-
   computer_choice = VALID_CHOICES.sample
 
-  prompt("You chose #{choice}, Computer chose: #{computer_choice}")
-
+  prompt("You chose #{choice}, computer chose: #{computer_choice}")
   display_results(choice, computer_choice)
-  update_players_score(choice, computer_choice, players_scores, player_wins, computer_wins)
+
+  update_players_score(choice, computer_choice, players_scores)
 
   prompt('----------')
   prompt("Your score: #{players_scores[:player]}")
   prompt("Computer's score: #{players_scores[:computer]}")
-  prompt("player win via local variable: #{player_wins}")
-  prompt("computer win via local variable: #{computer_wins}")
   prompt('----------')
 
+  # determine if program should be exited
   if players_scores[:player] == 5
-    prompt('You\'ve fucking won, congrats!!!!')
+    prompt('You\'ve won, congrats!!!!')
     exit
   elsif players_scores[:computer] == 5
     prompt('Computer has won ...')
